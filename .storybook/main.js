645887f19110416f9906a0bd8809cda44338a539
Masatoshi N <nishiguchi.masa@gmail.com>
2022-08-05 14:08:06 -0400

    Use SCSS within LitElement

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,51 @@
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  core: {
+    builder: 'webpack5'
+  },
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components"
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/web-components',
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'lit-scss-loader',
+          options: {
+            minify: true // defaults to false
+          }
+        },
+        { loader: 'extract-loader' },
+        { loader: 'css-loader' },
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [autoprefixer()]
+            }
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            // Prefer Dart Sass
+            implementation: require('sass'),
+
+            // See https://github.com/webpack-contrib/sass-loader/issues/804
+            webpackImporter: false,
+            sassOptions: {
+              includePaths: ['./node_modules']
+            }
+          }
+        }
+      ]
+    })
+
+    return config
+  }
 }
--- stories/my-button.js
+++ stories/my-button.js
@@ -1,38 +1,11 @@
 import { LitElement, html, css } from 'lit'
 import clsx from 'clsx'
+import Style from './my-button.scss'
 
 export class Button extends LitElement {
-  static styles = css`
-    .my-button {
-      font-weight: 700;
-      border: 0;
-      border-radius: 3em;
-      cursor: pointer;
-      display: inline-block;
-      line-height: 1;
-    }
-    .my-button--primary {
-      color: white;
-      background-color: #1ea7fd;
-    }
-    .my-button--secondary {
-      color: #333;
-      background-color: transparent;
-      box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
-    }
-    .my-button--small {
-      font-size: 12px;
-      padding: 10px 16px;
-    }
-    .my-button--medium {
-      font-size: 14px;
-      padding: 11px 20px;
-    }
-    .my-button--large {
-      font-size: 16px;
-      padding: 12px 24px;
-    }
-  `
+  static get styles() {
+    return [Style]
+  }
 
   static properties = {
     primary: { type: Boolean },
